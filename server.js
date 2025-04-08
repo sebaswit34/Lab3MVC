aa5fda260503bcdb8af70739f02d4673ebffd0b4
sebaswit34 <sebastian.swit.2003@o2.pl>
2025-04-08 18:20:41 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,44 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const renderNewProductPage = require("./views/renderNewProductPage.");
+const { PORT } = require("./config");
+const logger = require("./utils/logger");
+const productRoutes = require("./routing/product");
+const logoutRoutes = require("./routing/logout");
+const killRoutes = require("./routing/kill");
+const homeRoutes = require("./routing/home");
+const { STATUS_CODE } = require("./constants/statusCode");
+
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((request, _response, next) => {
+    const { url, method } = request;
+
+    logger.getInfoLog(url, method);
+    next();
+});
+
+app.use("/product", productRoutes);
+app.use("/logout", logoutRoutes);
+app.use("/kill", killRoutes);
+app.use(homeRoutes);
+app.use((request, response) => {
+    const { url } = request;
+
+    response
+        .status(STATUS_CODE.NOT_FOUND)
+        .sendFile(path.join(__dirname, "./views", "404.html"));
+    logger.getErrorLog(url);
+});
+
+app.listen(PORT);
